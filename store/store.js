b0c0a0c94310f8401f7b98bc4cb433abd5a7bf8b
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2023-05-28 19:38:48 +1000

    RW-3 Create Location Store

--- store/store.js
+++ store/store.js
@@ -1,10 +1,16 @@
 import { create } from 'zustand';
 import { HORIZONTAL_CARDS, VERTICAL_LIST } from '../config/constants';
+
 export const useWeatherStore = create((set) => ({
   forecast: {},
   current: {},
   weekly: [],
-  setAllForecast: (forecast) => set((state) => ({ forecast: forecast, current: forecast.current, weekly: forecast.weekly })),
+  setAllForecast: (forecast) =>
+    set((state) => ({
+      forecast: forecast,
+      current: forecast.current,
+      weekly: forecast.weekly,
+    })),
 }));
 
 const defaultHourlyState = HORIZONTAL_CARDS;
@@ -12,6 +18,22 @@ const defaultWeeklyState = VERTICAL_LIST;
 export const useSettingStore = create((set) => ({
   hourlyViewType: defaultHourlyState,
   weeklyViewType: defaultWeeklyState,
-  setHourlyViewType: (hourlyViewType) => set(() => ({ hourlyViewType: hourlyViewType })),
-  setWeeklyViewType: (weeklyViewType) => set(() => ({ weeklyViewType: weeklyViewType })),
+  setHourlyViewType: (hourlyViewType) =>
+    set(() => ({ hourlyViewType: hourlyViewType })),
+  setWeeklyViewType: (weeklyViewType) =>
+    set(() => ({ weeklyViewType: weeklyViewType })),
+}));
+
+export const useLocationStore = create((set) => ({
+  locations: [],
+  defaultLocation: {},
+  addNewLocation: (locationData) =>
+    set(({ state }) => ({ locations: [...state.locations, locationData] })),
+  removeLocation: (locationId) =>
+    set((state) => ({
+      locations: state.locations.filter(
+        (location) => location.id !== locationId
+      ),
+    })),
+  setDefaultLocation: (locationData) => set(() => locationData),
 }));
