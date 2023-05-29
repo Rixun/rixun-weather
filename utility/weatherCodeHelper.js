d3e06c34ce1dbe98ee5bf640f2586244f6b50cc5
Afx31 <post4wjm@gmail.com>
2023-05-29 21:49:58 +1000

    Setup all Weather Code enums

--- utility/weatherCodeHelper.js
+++ utility/weatherCodeHelper.js
@@ -1,12 +1,33 @@
 export const getWeatherIcon = (weatherCode) => {
-  const weatherCodeMap = {
+  const WeatherCodeEnum = {
     0: 'Sun', // Clear Sky
     1: 'Sun', // Mainly clear
-    2: 'SunCloud', // partly cloudy
-    3: 'SunCloud', // overcast
-    80: 'CloudLightRain', // Slight rain shower
-    81: 'CloudRain', // Moderate rain shower
-    82: 'CloudRain', // Heavy rain shower
+    2: 'SunCloud', // Partly cloudy
+    3: 'SunCloud', // Overcast
+    45: 'Fog', // Fog
+    48: 'Fog', // Depositing rime fog
+    51: 'CloudLightRain', // Drizzle: Light
+    53: 'CloudRain', // Drizzle: Moderate
+    55: 'CloudRain', // Drizzle: Dense
+    56: 'CloudLightRain', // Freezing Drizzle: Light
+    57: 'CloudRain', // Freezing Drizzle: Dense
+    61: 'RaindropEmpty', // Rain: Slight
+    63: 'RaindropFull', // Rain: Moderate
+    65: 'RaindropFull', // Rain: Heavy
+    66: 'RaindropFull', // Freezing Rain: Light
+    67: 'RaindropFull', // Freezing Rain: Heavy
+    71: 'CloudSnow', // Snow fall: Slight
+    73: 'CloudSnow', // Snow fall: Moderate
+    75: 'CloudSnow', // Snow fall: Heavy
+    77: 'SunCloudSnow', // Snow Grains
+    80: 'CloudLightRain', // Rain showers: Slight
+    81: 'CloudRain', // Rain showers: Moderate
+    82: 'CloudRain', // Rain showers: Heavy
+    85: 'SunCloudSnow', // Snow showers: Slight
+    86: 'SunCloudSnow', // Snow showers: Heavy
+    95: 'CloudRainThunder', // Thunderstorm: Slight or Moderate
+    96: 'CloudRainThunder', // Thunderstorm w/Hail: Slight
+    99: 'CloudRainThunder' // Thunderstorm w/Hail: Heavy
   };
-  return weatherCodeMap[weatherCode];
+  return WeatherCodeEnum[weatherCode];
 };
