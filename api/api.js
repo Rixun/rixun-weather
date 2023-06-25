6f2759e9d902146636f52e1d78db761b29559232
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2023-06-25 23:11:27 +1000

    RW-12 cleanup API functions, add rain chance

--- api/api.js
+++ api/api.js
@@ -1,93 +1,81 @@
 import jsonData from './suburbs.json';
 let SEARCH_BY = 'suburb'; // make configurable later
 
-const weeklyForecast = async (long, lat) => {
-  let returnResult = {};
-  let currentForecast = {};
-  let dailyHourlyForecast = [];
-  let weeklyForecast = [];
+const getCurrentRoundedDownHourIndex = () => {
+  const currentHour = new Date();
+  currentHour.setMinutes(0, 0, 0);
+  return currentHour.getHours();
+};
 
-  let res = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&timezone=Australia%2FSydney&current_weather=true&daily=weathercode&daily=temperature_2m_max,temperature_2m_min,uv_index_max,uv_index_clear_sky_max&hourly=temperature_2m,relativehumidity_2m`
+const mapCurrentForecast = (currentWeather, hourlyWeather, dailyWeather) => {
+  const currentForecast = {};
+  currentForecast.temperature = currentWeather.temperature;
+  currentForecast.windSpeed = currentWeather.windspeed;
+  currentForecast.weatherCode = currentWeather.weathercode;
+  const hourlyIndex = getCurrentRoundedDownHourIndex();
+  currentForecast.humidity = hourlyWeather['relativehumidity_2m'][hourlyIndex];
+  currentForecast.uv = dailyWeather['uv_index_max'][0];
+  currentForecast.uvClearSky = dailyWeather['uv_index_clear_sky_max'][0];
+  return currentForecast;
+};
+const mapHourlyForecast = (hourlyWeather) => {
+  const hourlyForecast = [];
+  // Future task: Get 24 hr hourly data from current hour, instead of just 0-24
+  for (let i = 0; i <= 24; i++) {
+    hourlyForecast.push({
+      temperature: hourlyWeather['temperature_2m'][i],
+      humidity: hourlyWeather['relativehumidity_2m'][i],
+      precipitation_probability: hourlyWeather['precipitation_probability'][i],
+    });
+  }
+  return hourlyForecast;
+};
+const mapDailyForecast = (dailyWeather) => {
+  const dailyForecast = [];
+  // Future task: Get 24 hr hourly data from current hour, instead of just 0-24
+  for (let i = 0; i < dailyWeather['time'].length; i++) {
+    dailyForecast.push({
+      date: dailyWeather['time'][i],
+      weatherCode: dailyWeather['weathercode'][i],
+      tempMax: dailyWeather['temperature_2m_max'][i],
+      tempMin: dailyWeather['temperature_2m_min'][i],
+      uv: dailyWeather['uv_index_max'][i],
+      uvClearSky: dailyWeather['uv_index_clear_sky_max'][i],
+    });
+  }
+  return dailyForecast;
+};
+
+export async function getWeatherForecast(locationData) {
+  let currentForecast = {};
+  let hourlyForecast = [];
+  let dailyForecast = [];
+
+  let response = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${locationData.latitude}&longitude=${locationData.longitude}&timezone=Australia%2FSydney&current_weather=true&daily=weathercode&daily=temperature_2m_max,temperature_2m_min,uv_index_max,uv_index_clear_sky_max&hourly=temperature_2m,relativehumidity_2m,precipitation_probability`
   );
 
-  // Hardcoded suburb for testing
-  // let res = await fetch(
-  //   `https://api.open-meteo.com/v1/forecast?latitude=-33.87&longitude=151.21&timezone=Australia%2FSydney&current_weather=true&daily=weathercode&daily=temperature_2m_max,temperature_2m_min,uv_index_max,uv_index_clear_sky_max&hourly=temperature_2m,relativehumidity_2m`
-  // );
-  let resData = await res.json();
+  let resData = await response.json();
 
-  if (res.status === 200) {
-    // Current forecast
-    currentForecast.temperature = resData['current_weather'].temperature;
-    currentForecast.windSpeed = resData['current_weather'].windspeed;
-    currentForecast.weatherCode = resData['current_weather'].weathercode;
-
-    // Daily hourly forecast
-    for (let i = 0; i < resData['hourly']['time'].length; i++) {
-      if (i === 24) {
-        break;
-      }
-
-      // To get the current humidity
-      if (i === 0) {
-        currentForecast.humidity = resData['hourly']['relativehumidity_2m'][i];
-      }
-
-      let hourlyResult = {};
-      hourlyResult.temperature = resData['hourly']['temperature_2m'][i];
-      hourlyResult.humidity = resData['hourly']['relativehumidity_2m'][i];
-      dailyHourlyForecast.push(hourlyResult);
-    }
-
-    // The next 7 days forecast
-    for (let i = 0; i < resData['daily']['time'].length; i++) {
-      let dailyResult = {};
-      dailyResult.date = resData['daily']['time'][i];
-      dailyResult.weatherCode = resData['daily']['weathercode'][i];
-      dailyResult.tempMax = resData['daily']['temperature_2m_max'][i];
-      dailyResult.tempMin = resData['daily']['temperature_2m_min'][i];
-      dailyResult.uv = resData['daily']['uv_index_max'][i];
-      dailyResult.uvClearSky = resData['daily']['uv_index_clear_sky_max'][i];
-
-      // To get the current UV ratings
-      if (i === 0) {
-        currentForecast.uv = resData['daily']['uv_index_max'][i];
-        currentForecast.uvClearSky =
-          resData['daily']['uv_index_clear_sky_max'][i];
-      }
-
-      weeklyForecast.push(dailyResult);
-    }
-
-    returnResult.current = currentForecast;
-    returnResult.dailyHourly = dailyHourlyForecast;
-    returnResult.weekly = weeklyForecast;
-
-    return returnResult;
+  if (response.status === 200) {
+    currentForecast = mapCurrentForecast(
+      resData['current_weather'],
+      resData['hourly'],
+      resData['daily']
+    );
+    hourlyForecast = mapHourlyForecast(resData['hourly']);
+    dailyForecast = mapDailyForecast(resData['daily']);
+    return {
+      current: currentForecast,
+      hourly: hourlyForecast,
+      daily: dailyForecast,
+    };
   } else {
     // error
     return {
       error: true,
     };
   }
-};
-
-export async function getWeatherForecast(locationData) {
-  // let searchForLatitude = '';
-  // let searchForLongitude = '';
-  console.log(locationData);
-
-  // for (let i = 0; i < jsonData.data.length; i++) {
-  //   if (jsonData.data[i][SEARCH_BY] == searchInput) {
-  //     //searchResults = jsonData.data[i];
-  //     searchForLatitude = jsonData.data[i].lat;
-  //     searchForLongitude = jsonData.data[i].long;
-  //     break;
-  //   }
-  // }
-
-  return await weeklyForecast(locationData.longitude, locationData.latitude);
 }
 
 export const getSuburbs = async (input) => {
@@ -100,7 +88,7 @@ export const getSuburbs = async (input) => {
   if (searchData.results) {
     return searchData.results;
   } else {
-    //error
+    // error
     return {
       error: true,
     };
