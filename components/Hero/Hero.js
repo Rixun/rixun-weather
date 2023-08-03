e88d2c1beb6da7e2cf52871c96142d90af2c144b
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2023-08-03 22:11:10 +1000

    RW-16 Update Hero Component Styling and Layout

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -17,48 +17,66 @@ export default function Hero() {
   return (
     <View style={styles.container}>
       {Object.keys(currentWeather).length > 0 && (
-        <View style={styles.currentWeatherSection}>
-          <Text style={styles.currentHeading}>{defaultLocation.name}</Text>
-          <Icon name={weatherIcon} size={150} />
-          <Text style={styles.currentData}>
-            Temperature: {currentWeather.temperature}
-          </Text>
-          <Text style={styles.currentData}>
-            Humidity: {currentWeather.humidity}
-          </Text>
-          <Text style={styles.currentData}>
-            Windspeed: {currentWeather.windSpeed}
-          </Text>
-          <Text style={styles.currentData}>UV: {currentWeather.uv}</Text>
-          <Text style={styles.currentData}>
-            UV - Clear Sky: {currentWeather.uvClearSky}
-          </Text>
-        </View>
+        <>
+          <View style={styles.headingContainer}>
+            <Text style={styles.currentHeading}>{defaultLocation.name}</Text>
+          </View>
+          <View style={styles.subContainer}>
+            <View style={styles.main}>
+              <Icon name={weatherIcon} size={150} />
+              <Text style={[styles.currentData, styles.temperature]}>
+                {Math.round(currentWeather.temperature)}°
+              </Text>
+            </View>
+            <View style={styles.info}>
+              <Text style={styles.currentData}>
+                Humidity: {currentWeather.humidity}
+              </Text>
+              <Text style={styles.currentData}>
+                Windspeed: {currentWeather.windSpeed}
+              </Text>
+              <Text style={styles.currentData}>UV: {currentWeather.uv}</Text>
+              <Text style={styles.currentData}>
+                UV - Clear Sky: {currentWeather.uvClearSky}
+              </Text>
+            </View>
+          </View>
+        </>
       )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  currentWeatherSection: {
+  container: {
+    width: '100%',
+    maxWidth: 768,
+  },
+  headingContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 10,
   },
   currentHeading: {
     color: '#fff',
     fontSize: 20,
   },
+  subContainer: {
+    width: '100%',
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingRight: 25,
+  },
+  temperature: {
+    fontSize: 75,
+  },
+  info: {
+    paddingLeft: 25,
+  },
   currentData: {
     color: '#fff',
-    fontSize: 15,
-  },
-  textInput: {
-    height: 40,
-    width: 350,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+    fontSize: 14,
   },
 });
